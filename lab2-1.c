ecd683771b6defe7563ec3cfdef5d7d467201915
needsomecash <needsomecash@naver.com>
2021-03-15 22:01:45 +0900

    data structure hw#2

---
+++ lab2-1.c
@@ -0,0 +1,35 @@
+/*#include <stdio.h>
+int main()
+{
+	char charType; //char 타입 변수 선언
+	int integerType; //int 타입 변수 선언
+	float floatType;//float 타입 변수 선언
+	double doubleType;//double 타입 변수 선언
+
+	printf("[---- [이정민]  [2018038030] ----]\n");//이름, 학번 출력
+
+	printf("Size of char: %ld bytes\n",sizeof(charType));//charType 변수의 크기
+	printf("Size of int: %ld bytes\n",sizeof(integerType));//IntegerType 변수의 크기
+	printf("Size of float: %ld bytes\n",sizeof(floatType));//floatType 변수의 크기
+	printf("Size of double: %ld bytes\n",sizeof(doubleType));//doubleType 변수의 크기
+
+	printf("--------------------------------------------------\n");
+
+	printf("Size of char: %ld bytes\n",sizeof(char));//char 타입의 크기
+	printf("Size of int: %ld bytes\n",sizeof(int));//int 타입의 크기
+	printf("Size of float: %ld bytes\n",sizeof(float));//float 타입의 크기
+	printf("Size of double: %ld bytes\n",sizeof(double));//double 타입의 크기
+
+	printf("--------------------------------------------------\n");
+
+	printf("Size of char*: %ld bytes\n",sizeof(char*));//char* 타입의 크기
+	printf("Size of int*: %ld bytes\n",sizeof(int*));//int* 타입의 크기
+	printf("Size of float*: %ld bytes\n",sizeof(float*));//float* 타입의 크기
+	printf("Size of double*: %ld bytes\n",sizeof(double*));//double* 타입의 크기
+
+	return 0;
+
+
+
+}*/
+
